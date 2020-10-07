2b27db020f8e9aa901645541fa17cc4b5a5701ef
Iamsiyanda <s.mayekiso@gmail.com>
2020-10-07 16:46:07 +0200

    Updates to document

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Function Commands
+
 function createCityList(citySearchList) {
     $("#city-list").empty();
   
@@ -17,3 +19,15 @@ function createCityList(citySearchList) {
       $("#city-list").append(cityListEntry);
     }
   }
+
+  // API
+
+  function populateCityWeather(city, citySearchList) {
+    createCityList(citySearchList);
+  
+    var queryURL =
+      "https:api.openweathermap.org/data/2.5/weather?q={city name}&appid={0d1c817b4d550379fbd5478824849526}"+
+      city;
+  
+    
+    });
